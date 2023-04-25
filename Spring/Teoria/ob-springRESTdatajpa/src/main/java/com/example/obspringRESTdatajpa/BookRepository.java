9fa6c6a5c95a69b91577ec80865315ada578a3ca
Roberto Alcazar Morales <roberto.am96@hotmail.com>
2023-04-25 12:36:38 +0200

    Comenzado proyecto REST (Teoria)

---
+++ Spring/Teoria/ob-springRESTdatajpa/src/main/java/com/example/obspringRESTdatajpa/BookRepository.java
@@ -0,0 +1,9 @@
+package com.example.obspringRESTdatajpa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book,Long> {
+
+}
