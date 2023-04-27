bd211de4eee397fcf31ccb1f0fd0fca2be331927
Roberto Alcazar Morales <roberto.am96@hotmail.com>
2023-04-27 14:46:17 +0200

    Tema 7

--- Spring/Teoria/ob-springRESTdatajpa/src/main/java/com/example/obspringRESTdatajpa/controllers/BookController.java
+++ Spring/Teoria/ob-springRESTdatajpa/src/main/java/com/example/obspringRESTdatajpa/controllers/BookController.java
@@ -28,33 +28,50 @@ public class BookController {
        return repository.findAll();
     }
 
-    // buscar un libro en base a su id
+    /**
+     * buscar un libro en base a su id
+     * @param id
+     * @return Response entity ok o 404 error not found
+      */
+
     @GetMapping("/api/books/{id}")
     public ResponseEntity<Book> findById(@PathVariable Long id) {
-        if (repository.findById(id).isEmpty())
+        if (!repository.existsById(id))
             return ResponseEntity.notFound().build();
         return ResponseEntity.ok(repository.findById(id).get());
     }
     //crear libro en base de datos
     @PostMapping("/api/books")
     public ResponseEntity<Book> create(@RequestBody Book b) {   //pide un cuerpo a la url
-
-        for (Long i = 0L; i < repository.count(); i++) {
-            if (repository.getReferenceById(i).getId().equals(b.getId())) {
-                return ResponseEntity.noContent().build();
-            }
-        }
+        if (b.getId() != null) // es una creacion
+            return ResponseEntity.badRequest().build(); // intenta crear un libro con una id existente
         return ResponseEntity.ok(repository.save(b));
     }
     //actualizar un libro en base de datos
-    @PutMapping("/api/books/update/{id}")
-    public Book update(@PathVariable Long id,@RequestBody Book b) {
-        b.setId(id);
-        return b;
+    @PutMapping("/api/books")
+    public ResponseEntity<Book> update(@RequestBody Book b) {
+        if (b.getId() == null)// no es una creacion
+            return ResponseEntity.noContent().build();
+
+        if (!repository.existsById(b.getId())) { //si no existe, lanza un error de not found
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(repository.save(b));
     }
     //borrar un libro en base de datos
     @DeleteMapping("/api/books/{id}")
-    public void delete(@PathVariable Long id) {
-        repository.deleteById(id);
+    public ResponseEntity<Book> delete(@PathVariable Long id) {
+
+        if (repository.existsById(id)) {
+            repository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+
+    }
+    @DeleteMapping("/api/books")
+    public ResponseEntity<Book> deleteAll(){
+        repository.deleteAll();
+        return ResponseEntity.noContent().build();
     }
 }
