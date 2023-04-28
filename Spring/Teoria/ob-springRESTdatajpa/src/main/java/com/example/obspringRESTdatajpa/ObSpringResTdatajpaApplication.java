17f1384b76f60ed333d941783318fc8e80e21634
Roberto Alcazar Morales <roberto.am96@hotmail.com>
2023-04-28 14:24:25 +0200

    Tema 8, añadida documentacion swagger pero no funciona porque no encuentra una referencia automaticamente.

--- Spring/Teoria/ob-springRESTdatajpa/src/main/java/com/example/obspringRESTdatajpa/ObSpringResTdatajpaApplication.java
+++ Spring/Teoria/ob-springRESTdatajpa/src/main/java/com/example/obspringRESTdatajpa/ObSpringResTdatajpaApplication.java
@@ -5,6 +5,8 @@ import com.example.obspringRESTdatajpa.repositories.BookRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
 
 import java.time.LocalDate;
 
---
+++ Spring/Teoria/ob-springRESTdatajpa/src/main/java/com/example/obspringRESTdatajpa/config/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package com.example.obspringRESTdatajpa.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+/**
+ * Configuracion Swagger para la generacion de documentacion de la API REST
+ *
+ * http://localhost:8080/swagger-ui/
+ */
+@Configuration
+public class SwaggerConfig {
+
+    @Bean   //disponible en el contenedor de Beans de spring
+    public Docket api(){
+
+        return new Docket(DocumentationType.SWAGGER_12)
+                .apiInfo(apiDetails())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+    private ApiInfo apiDetails(){
+        return new ApiInfo("Spring Boot Books API REST",
+                "Library api rest docs",
+                "1.0",
+                "http://google.com",
+                new Contact("Roberto", "http://google.com", "loquesea@example.com"),
+                "License",
+                "http://google.com",
+                Collections.emptyList());
+    }
+}
